7ed8d80574590fc1b6ff69098c3d4c147fbead59
zaia <ahadkhan98@hotmail.com>
2019-05-28 21:30:16 -0400

    - Set up database - Migrated to database - Added functionality for automatically showing all projects

--- portfolio-project/home/views.py
+++ portfolio-project/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    projects = Project.objects
+    return render(request, 'home/home.html', {'projects':projects})
