0a19c16b4ce3bd1134cb77d73b6691b56708e360
Zai <zaia@iu.edu>
2019-05-29 12:06:43 -0400

    now able to display pictures on posts

--- portfolio-project/portfolio/urls.py
+++ portfolio-project/portfolio/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects', projects.views.project, name='projects'),
     path('',home.views.home, name='home'),
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
