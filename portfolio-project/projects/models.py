38c771a84a35da80bbb0f6a6f8454ef065dc97cc
Zai <zaia@iu.edu>
2019-05-29 14:35:27 -0400

    changed the way you post projects

--- portfolio-project/projects/models.py
+++ portfolio-project/projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=25)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
