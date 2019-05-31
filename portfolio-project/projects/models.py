0b460f16d28301bb450f862f4aaa18c2d628b01a
Zai <zaia@iu.edu>
2019-05-31 14:26:01 -0400

    Added work experience timeline

--- portfolio-project/projects/models.py
+++ portfolio-project/projects/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=25)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=100)
+    skills = models.CharField(max_length=50)
     description = models.TextField()
 
     def __str__(self):
