f6ba47adf2846051047f043e9e9884d7e2f14964
Zai <zaia@iu.edu>
2019-06-03 15:51:37 -0400

    adjusted max size in models.py and added all my awards

--- portfolio-project/projects/models.py
+++ portfolio-project/projects/models.py
@@ -9,16 +9,16 @@ class Project(models.Model):
     description = models.TextField()
 
 class Job(models.Model):
-    title = models.CharField(max_length=50)
-    site = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
-    date = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    site = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date = models.CharField(max_length=100)
     description = models.TextField()
 
 class Certificate(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/certificates/')
-    summary = models.CharField(max_length=50)
+    summary = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
 
     def __str__(self):
