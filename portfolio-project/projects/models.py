85c5d43ff04bd0e95cbc6808eb36526979f3dbb9
Zai <zaia@iu.edu>
2019-06-03 15:08:20 -0400

    added certificates & jobs model

--- portfolio-project/projects/models.py
+++ portfolio-project/projects/models.py
@@ -8,5 +8,18 @@ class Project(models.Model):
     skills = models.CharField(max_length=50)
     description = models.TextField()
 
+class Job(models.Model):
+    title = models.CharField(max_length=50)
+    site = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Certificate(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/certificates/')
+    summary = models.CharField(max_length=50)
+    link = models.CharField(max_length=100)
+
     def __str__(self):
         return self.title
